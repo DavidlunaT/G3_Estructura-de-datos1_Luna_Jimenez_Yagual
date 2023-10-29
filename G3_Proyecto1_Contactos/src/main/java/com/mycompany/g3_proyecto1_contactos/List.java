e5733a11c9192b4fcaa55fec7da89358031c22a0
unknown <daluna@espol.edu.ec>
2023-10-29 02:12:39 -0500

    Implementacion de la interfaz List, y las clases ArrayList y CustomCircularIterator

---
+++ G3_Proyecto1_Contactos/src/main/java/com/mycompany/g3_proyecto1_contactos/List.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.g3_proyecto1_contactos;
+
+/**
+ *
+ * @author David
+ */
+public interface List {
+    
+}
