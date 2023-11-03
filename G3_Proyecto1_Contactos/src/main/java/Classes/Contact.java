c756358e8e26b36c5104647245a5e577bfbef810
gabsjimz <gabdejim@espol.edu.ec>
2023-11-03 18:26:20 -0500

    Nuevo paquete

--- G3_Proyecto1_Contactos/src/main/java/Classes/Contact.java
+++ G3_Proyecto1_Contactos/src/main/java/Classes/Contact.java
@@ -40,33 +40,4 @@ public abstract class Contact implements Serializable{
         this.phones = phones;
     }
     
-    public static ArrayList load(ArrayList<Contact> contacts) {
-        
-       //leer la lista de contactos del archivo serializado
-        try (ObjectInputStream oi = new ObjectInputStream(new FileInputStream("ruta"))) {
-            contacts = (ArrayList<Contact>) oi.readObject();            
-            
-        } catch (FileNotFoundException ex) {
-            System.out.println("archivo no existe");
-        } catch (IOException ex) {
-            System.out.println("error io:"+ex.getMessage());
-        } catch (ClassNotFoundException ex) {
-            System.out.println("error class:"+ex.getMessage());
-        } 
-        return contacts;
-    }
-    
-    public static void save(Contact c, ArrayList<Contact> contacts){
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("archivo"))){
-            contacts.add(c);
-            out.writeObject(contacts);
-        }catch(FileNotFoundException e1){
-            System.out.println(e1);
-        }catch(IOException e2){
-            System.out.println(e2);
-        }catch(Exception e3){
-            System.out.println(e3);
-        }
-    }
-    
 }
