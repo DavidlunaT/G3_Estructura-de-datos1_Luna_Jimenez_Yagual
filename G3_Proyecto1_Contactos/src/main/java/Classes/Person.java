73652377030826a8930e8cd381d69752f37b4cc6
unknown <daluna@espol.edu.ec>
2023-11-03 20:10:46 -0500

    base de ContactVisualization

--- G3_Proyecto1_Contactos/src/main/java/Classes/Person.java
+++ G3_Proyecto1_Contactos/src/main/java/Classes/Person.java
@@ -15,36 +15,45 @@ import Interfaces.List;
  */
 public class Person extends Contact{ 
     
-    public String secondName;
-    public String lastName;
-    public String secondLastName;
-
+    public String firstName1;
+    public String firstName2;
+    public String lastName1;
+    public String lastName2;
+    
     public Person(String name, List<Phone> phones) {
         super(name, phones);
     }
 
-    public String getSecondName() {
-        return secondName;
+    public String getFirstName1() {
+        return firstName1;
     }
 
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
+    public void setFirstName1(String firstName1) {
+        this.firstName1 = firstName1;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getFirstName2() {
+        return firstName2;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setFirstName2(String firstName2) {
+        this.firstName2 = firstName2;
     }
 
-    public String getSecondLastName() {
-        return secondLastName;
+    public String getLastName1() {
+        return lastName1;
     }
 
-    public void setSecondLastName(String secondLastName) {
-        this.secondLastName = secondLastName;
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
     }
 
     public String getName() {
@@ -110,6 +119,7 @@ public class Person extends Contact{
     public void setPhoto(String Photo) {
         this.Photo = Photo;
     }
+
 
 
     
--- G3_Proyecto1_Contactos/src/main/java/module-info.java
+++ G3_Proyecto1_Contactos/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.g3_proyecto1_contactos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.g3_proyecto1_contactos to javafx.fxml;
     exports com.mycompany.g3_proyecto1_contactos;
